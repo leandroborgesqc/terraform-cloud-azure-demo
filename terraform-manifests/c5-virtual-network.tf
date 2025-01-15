35fc586b8c380a1b38963605078503c72867b549
Leandro Borges <leandroborges_br@hotmail.com>
2025-01-15 13:37:21 -0500

    Enable private endpoint network policies for subnet in c5-virtual-network.tf

--- terraform-manifests/c5-virtual-network.tf
+++ terraform-manifests/c5-virtual-network.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "mysubnet" {
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
-  private_endpoint_network_policies = "Disabled"
+  private_endpoint_network_policies = "Enabled"
 }
 
 # Create Public IP Address
